012d76ed442e9d62f80eb33c323a3fada57283a5
Sigmanificient <edhyjox@gmail.com>
2024-12-30 13:28:14 +0100

    Add version flag for lsp info

--- src/ehcsls/__main__.py
+++ src/ehcsls/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 from . import __version__, server
 
@@ -7,6 +8,10 @@ logger = logging.getLogger(__package__)
 
 def main():
     logger.info("hello from EHCSLS", __version__)
+    if "--version"in sys.argv[1::]:
+        print(__version__)
+        return os.EX_OK;
+
     server.start_io()
 
 
